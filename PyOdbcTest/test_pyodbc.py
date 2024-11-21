2633ccb07e568aae4d5ada242e98af54c14816ed
hador1995 <wojciech.bojewski@outlook.com>
2024-11-21 21:43:20 +0100

    Unittest init

--- PyOdbcTest/test_pyodbc.py
+++ PyOdbcTest/test_pyodbc.py
@@ -4,7 +4,7 @@ import pyodbc
 import logging
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename='pyodbc_test.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filemode="w")
+logging.basicConfig(filename='pyodbc_test.log', level=logging.INFO, format='%(asctime)s - %(message)s', filemode="w")
 
 class TestPyOdbcTest(unittest.TestCase):
     def test_pyodbc_version(self):
@@ -12,17 +12,18 @@ class TestPyOdbcTest(unittest.TestCase):
         with patch('pyodbc.version', new="x.y.z"):
             self.assertEqual("x.y.z", pyodbc.version)
 
+    def test_pyodbc_dataSources(self):
+        logger.info(f"PyOdbc dataSources: {pyodbc.dataSources()}")
+        with patch('pyodbc.dataSources') as mock_dataSources:
+            mock_dataSources.return_value={'name':'driver'}
+            self.assertEqual({'name':'driver'}, pyodbc.dataSources())
+
     def test_pyodbc_drivers(self):
         logger.info(f"PyOdbc drivers: {pyodbc.drivers()}")
         with patch('pyodbc.drivers') as mock_drivers:
             mock_drivers.return_value=['driver1','driver2']
             self.assertEqual(['driver1','driver2'], pyodbc.drivers())
 
-    def test_pyodbc_dataSources(self):
-        logger.info(f"PyOdbc dataSources: {pyodbc.dataSources()}")
-        with patch('pyodbc.dataSources') as mock_dataSources:
-            mock_dataSources.return_value={'name':'driver'}
-            self.assertEqual({'name':'driver'}, pyodbc.dataSources())
 
 if __name__ == '__main__': 
     unittest.main()
---
+++ UnittestTest/test_unittest.py
@@ -0,0 +1,5 @@
+import unittest
+
+class UnittestTest(unittest.TestCase):
+    def test_unittest_enable(self):
+        self.fail()
