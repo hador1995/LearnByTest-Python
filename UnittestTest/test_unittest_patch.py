ff6a9f12bac3260482dc2cd343f941829b45961c
hador1995 <wojciech.bojewski@outlook.com>
2024-11-24 17:35:02 +0100

    Unittest: mock_open

--- UnittestTest/test_unittest_patch.py
+++ UnittestTest/test_unittest_patch.py
@@ -1,12 +1,12 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import Mock, patch, mock_open
 import logging
 import sys
 
 logger = logging.getLogger(__name__)
 
 class UnittestPatchTest(unittest.TestCase):
-    def test_patch_builtins_function(self):
+    def test_patch_builtins_function_print(self):
         with patch("builtins.print") as mock_print:
 
             print("Hello world", end="\n");
@@ -18,4 +18,32 @@ class UnittestPatchTest(unittest.TestCase):
             args, kwargs = mock_print.call_args
             self.assertEqual(('print error',) , args)
             self.assertEqual({'file': sys.stderr} , kwargs)
+
+    def test_patch_builtins_function_open_context_manager(self):
+        with patch("builtins.open") as mock_open:
+            mock_file = Mock()
+            mock_file.readlines.return_value = ['a\n', '1\n', 'False\n']                        
+            mock_open.return_value.__enter__.return_value= mock_file
+            with open("test.txt", "r") as file:
+                self.assertListEqual(['a\n', '1\n', 'False\n'], file.readlines())
+
+    def test_patch_builtins_function_open(self):
+        with patch("builtins.open") as mock_open:
+            mock_file = Mock()
+            mock_file.readlines.return_value = ['a\n', '1\n', 'False\n']            
+            mock_open.return_value = mock_file            
+            file = open("test.txt", "r")
+            self.assertListEqual(['a\n', '1\n', 'False\n'], file.readlines())
+
+    def test_patch_builtins_function_mock_open(self):
+        with patch("builtins.open", mock_open(read_data="a\n1\nFalse\n") ):
+
+            file = open("test.txt", "r")
+            self.assertListEqual(['a\n', '1\n', 'False\n'], file.readlines())
+
+            with open("test.txt", "r") as file:
+                self.assertListEqual(['a\n', '1\n', 'False\n'], file.readlines())
+
+if __name__ == "__main__":
+    unittest.main()
 
